d6f6501ab34983efb5765eacaf682b0bb4f4b064
asnr <asnr@users.noreply.github.com>
2019-11-24 18:01:26 -0800

    Don't throw an exception on empty code input

--- lambda_calculus_parser/parse_lambda_expression.hs
+++ lambda_calculus_parser/parse_lambda_expression.hs
@@ -7,6 +7,9 @@ main = TIO.getContents >>= putStr . show . parse . T.filter (not . isWhitespace)
 isWhitespace :: Char -> Bool
 isWhitespace c = c == ' ' || c == '\n'
 
+headMay :: T.Text -> Maybe Char
+headMay t = if T.null t then Nothing else Just $ T.head t
+
 -- We are going to ignore lexing for now, so all variable names will be single
 -- letters in a-z. Lambdas will be represented by /.
 data Expr = Var Char | App Expr Expr deriving (Show)
@@ -14,9 +17,12 @@ data Expr = Var Char | App Expr Expr deriving (Show)
 data ParseResult = ParseResult { expr :: Expr, unparsedCode :: T.Text } deriving (Show)
 
 parseVar :: T.Text -> Maybe ParseResult
-parseVar code = if T.head code >= 'a' && T.head code <= 'z'
-  then Just $ ParseResult (Var (T.head code)) (T.tail code)
-  else Nothing
+parseVar code = let headChar = headMay code in
+  case headChar of
+    Nothing -> Nothing
+    Just c -> if c >= 'a' && c <= 'z'
+      then Just $ ParseResult (Var c) (T.tail code)
+      else Nothing
 
 parseApp :: T.Text -> Maybe ParseResult
 parseApp code = let maybeResult = parseVar code in
