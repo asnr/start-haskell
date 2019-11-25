0060cc09e9c8d1f39ecdb2d635418d1c94a94048
asnr <asnr@users.noreply.github.com>
2019-11-25 07:57:47 -0800

    Refactor: use the maybe monad to simplify parsing code

--- lambda_calculus_parser/parse_lambda_expression.hs
+++ lambda_calculus_parser/parse_lambda_expression.hs
@@ -17,28 +17,21 @@ data Expr = Var Char | App Expr Expr deriving (Show)
 data ParseResult = ParseResult { expr :: Expr, unparsedCode :: T.Text } deriving (Show)
 
 parseVar :: T.Text -> Maybe ParseResult
-parseVar code = let headChar = headMay code in
-  case headChar of
-    Nothing -> Nothing
-    Just c -> if c >= 'a' && c <= 'z'
-      then Just $ ParseResult (Var c) (T.tail code)
-      else Nothing
+parseVar code = do {
+  c <- headMay code;
+  if c >= 'a' && c <= 'z'
+    then Just $ ParseResult (Var c) (T.tail code)
+    else Nothing
+  }
 
 parseApp :: T.Text -> Maybe ParseResult
-parseApp code = let maybeCode = consumeToken '(' code in
-  case maybeCode of
-    Nothing -> Nothing
-    Just codeAfterOpenBracket -> let maybeResult = parse codeAfterOpenBracket in
-      case maybeResult of
-        Nothing -> Nothing
-        Just result -> let maybeNextResult = parse $ unparsedCode result in
-          case maybeNextResult of
-            Nothing -> Nothing
-            Just nextResult ->
-              let maybeEndCode = consumeToken ')' $ unparsedCode nextResult in
-                case maybeEndCode of
-                  Nothing -> Nothing
-                  Just endCode -> Just $ ParseResult (App (expr result) (expr nextResult)) endCode
+parseApp code = do {
+  codeAfterOpenBracket <- consumeToken '(' code;
+  result <- parse codeAfterOpenBracket;
+  nextResult <- parse $ unparsedCode result;
+  remainingCode <- consumeToken ')' $ unparsedCode nextResult;
+  Just $ ParseResult (App (expr result) (expr nextResult)) remainingCode
+  }
 
 parse :: T.Text -> Maybe ParseResult
 parse code = let varParse = parseVar code in
@@ -47,12 +40,12 @@ parse code = let varParse = parseVar code in
     x -> x
 
 consumeToken :: Char -> T.Text -> Maybe T.Text
-consumeToken token code = let headChar = headMay code in
-  case headChar of
-    Nothing -> Nothing
-    Just char -> if char == token
-      then Just $ T.tail code
-      else Nothing
+consumeToken token code = do {
+  headChar <- headMay code;
+  if headChar == token
+    then Just $ T.tail code
+    else Nothing
+  }
 
 parseAll :: T.Text -> Maybe ParseResult
 parseAll code = let maybeResult = parse code in
