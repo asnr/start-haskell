afbc23cfd552a75f0f72a13347b8952ddc41f00a
asnr <asnr@users.noreply.github.com>
2019-11-26 08:24:17 -0800

    Refactor: don't use braces and semicolons in do statements

--- lambda_calculus_parser/parse_lambda_expression.hs
+++ lambda_calculus_parser/parse_lambda_expression.hs
@@ -18,45 +18,41 @@ data Expr = Var Char | App Expr Expr | Lam Char Expr deriving (Show)
 data ParseResult = ParseResult { expr :: Expr, unparsedCode :: T.Text } deriving (Show)
 
 parseVar :: T.Text -> Maybe ParseResult
-parseVar code = do {
-  c <- headMay code;
+parseVar code = do
+  c <- headMay code
   if c >= 'a' && c <= 'z'
     then Just $ ParseResult (Var c) (T.tail code)
     else Nothing
-  }
 
 parseApp :: T.Text -> Maybe ParseResult
-parseApp code = do {
-  codeAfterOpenBracket <- consumeToken '(' code;
-  result <- parse codeAfterOpenBracket;
-  nextResult <- parse $ unparsedCode result;
-  remainingCode <- consumeToken ')' $ unparsedCode nextResult;
+parseApp code = do
+  codeAfterOpenBracket <- consumeToken '(' code
+  result <- parse codeAfterOpenBracket
+  nextResult <- parse $ unparsedCode result
+  remainingCode <- consumeToken ')' $ unparsedCode nextResult
   Just $ ParseResult (App (expr result) (expr nextResult)) remainingCode
-  }
 
 parseLam :: T.Text -> Maybe ParseResult
-parseLam code = do {
-  codeAfterOpenBracket <- consumeToken '(' code;
-  codeAfterLambda <- consumeToken '\\' codeAfterOpenBracket;
-  varResult <- parseVar codeAfterLambda;
-  codeAfterPeriod <- consumeToken '.' $ unparsedCode varResult;
-  exprResult <- parse codeAfterPeriod;
-  codeAfterCloseBracket <- consumeToken ')' $ unparsedCode exprResult;
+parseLam code = do
+  codeAfterOpenBracket <- consumeToken '(' code
+  codeAfterLambda <- consumeToken '\\' codeAfterOpenBracket
+  varResult <- parseVar codeAfterLambda
+  codeAfterPeriod <- consumeToken '.' $ unparsedCode varResult
+  exprResult <- parse codeAfterPeriod
+  codeAfterCloseBracket <- consumeToken ')' $ unparsedCode exprResult
   case expr varResult of
     Var c -> Just $ ParseResult (Lam c (expr exprResult)) codeAfterCloseBracket
     _ -> Nothing
-  }
 
 parse :: T.Text -> Maybe ParseResult
 parse code = parseVar code <|> parseApp code <|> parseLam code
 
 consumeToken :: Char -> T.Text -> Maybe T.Text
-consumeToken token code = do {
-  headChar <- headMay code;
+consumeToken token code = do
+  headChar <- headMay code
   if headChar == token
     then Just $ T.tail code
     else Nothing
-  }
 
 parseAll :: T.Text -> Maybe ParseResult
 parseAll code = let maybeResult = parse code in
